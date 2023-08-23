22a77eea41d6dcdf808855352f8fa436a332ed8f
Armando Martinez <alemar.martinez16@gmail.com>
2023-08-23 18:38:49 -0300

    importacion de signiForm.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { auth } from "./app/firebase.js";
 import { loginCheck } from "./app/loginCheck.js";
 import "./app/logout.js";
 import "./app/singupForm.js";
+import "./app/signinForm.js";
 
 onAuthStateChanged(auth, async (user) => {
   loginCheck(user);
