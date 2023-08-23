ea5b225019521fdd145b5d9b6f0bf0c7b9eb9fcb
Armando Martinez <alemar.martinez16@gmail.com>
2023-08-23 14:50:18 -0300

    Se agrega estilo de alerta + logica de ToastifyJS

--- src/app/singupForm.js
+++ src/app/singupForm.js
@@ -1,5 +1,6 @@
 import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-auth.js";
 import { auth } from "./firebase.js";
+import { showMessage } from "./showMessage.js";
 
 const signupForm = document.querySelector("#signup-form");
 signupForm.addEventListener("submit", async (e) => {
@@ -19,21 +20,21 @@ signupForm.addEventListener("submit", async (e) => {
     // para cerrar el modal
     const sigupModal = document.querySelector("#singupModal");
     const modal = bootstrap.Modal.getInstance(sigupModal);
-
     modal.hide();
+
+    showMessage("Welcome" + userCredentials.user.email);
   } catch (error) {
     // console.log(error.message);
     // console.log(error.code);
 
-    if (error.code === 'auth/email-already-in-use') {
-      alert("The email address is already in use by another account.");
-    } else if (error.code === 'auth/invalid-email') {
-      alert("The email address is misspelled.");
-    } else if(error.code === 'auth/weak-password') {
-      alert("The password is too weak.");
-    }else if (error.code){
-      alert('something went wrong');
+    if (error.code === "auth/email-already-in-use") {
+      showMessage("Email already in use", "error");
+    } else if (error.code === "auth/invalid-email") {
+      showMessage("The email address is misspelled.", "error");
+    } else if (error.code === "auth/weak-password") {
+      showMessage("The password is too weak.", "error");
+    } else if (error.code) {
+      showMessage("something went wrong", "error");
     }
-
   }
 });
