61a6bd150beffabaead8d7843e368f5cad6af5e6
Armando Martinez <alemar.martinez16@gmail.com>
2023-08-23 14:49:40 -0300

    se crea showMessage

---
+++ src/app/showMessage.js
@@ -0,0 +1,23 @@
+
+export function showMessage(message, type = "success") {
+  Toastify({
+    text: message,
+    duration: 3000,
+    destination: "https://github.com/apvarun/toastify-js",
+    newWindow: true,
+    close: true,
+    gravity: "top", // `top` or `bottom`
+    position: "center", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      background:
+        type === "success"
+          ? "linear-gradient(to right, #00b09b, #96c93d)"
+          : "linear-gradient(to right, #ff0000, #ff7300)",
+    },
+    onClick: function () {}, // Callback after click
+  }).showToast();
+} 
+  
+
+
