207ed120ff11ef5037ddd3069745e759cb72f2e3
Armando Martinez <alemar.martinez16@gmail.com>
2023-08-23 13:05:48 -0300

    registro funcional con cierre de modal

--- src/app/singupForm.js
+++ src/app/singupForm.js
@@ -1,8 +1,27 @@
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-auth.js";
+import { auth } from "./firebase.js";
+
 const signupForm = document.querySelector("#signup-form");
-signupForm.addEventListener("submit", (e) => {
+signupForm.addEventListener("submit", async (e) => {
   e.preventDefault();
 
   const email = signupForm["signup-email"].value;
   const password = signupForm["signup-password"].value;
   console.log(email, password);
+
+  try {
+    const userCredentials = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    console.log(userCredentials);
+    // para cerrar el modal
+    const sigupModal = document.querySelector("#singupModal");
+    const modal = bootstrap.Modal.getInstance(sigupModal);
+
+    modal.hide();
+  } catch (error) {
+    console.log(error);
+  }
 });
