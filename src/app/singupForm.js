bdd1c0206495149c22a9c9965b2acbba4cea4ccd
Armando Martinez <alemar.martinez16@gmail.com>
2023-08-23 11:16:28 -0300

    definicion de variables email-passwork

--- src/app/singupForm.js
+++ src/app/singupForm.js
@@ -1,7 +1,8 @@
-
-const signupForm = document.querySelector('#signup-form');
-signupForm.addEventListener('submit', (e) => {
+const signupForm = document.querySelector("#signup-form");
+signupForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  
-})
+  const email = signupForm["signup-email"].value;
+  const password = signupForm["signup-password"].value;
+  console.log(email, password);
+});
