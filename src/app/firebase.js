9016169e96a541d13cfd15daa459ee22d445c755
Armando Martinez <alemar.martinez16@gmail.com>
2023-08-24 00:13:55 -0300

    se agrega funcion getFirestores para base de datos

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -3,6 +3,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.0/firebas
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-auth.js";
+import {getFirestore} from "https://www.gstatic.com/firebasejs/10.3.0/firebase-firestore.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -17,4 +18,5 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
 // console.log(auth);
