252034c77f865665095d62b8e8b7a8311561381b
Armando Martinez <alemar.martinez16@gmail.com>
2023-08-24 00:14:56 -0300

    se importan dunciones: getDocs, collection, db,

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
+import { getDocs, collection } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-firestore.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-auth.js";
-import { auth } from "./app/firebase.js";
+import { auth, db } from "./app/firebase.js";
 import { loginCheck } from "./app/loginCheck.js";
+import {setupPosts} from "./app/postList.js";
 import "./app/logout.js";
 import "./app/singupForm.js";
 import "./app/signinForm.js";
@@ -9,10 +11,12 @@ import "./app/facebookLogin.js";
 import "./app/githubLogin.js";
 
 onAuthStateChanged(auth, async (user) => {
+  if (user) {
+    const querySnapshot = await getDocs(collection(db, 'posts'))
+    setupPosts(querySnapshot.docs)
+    // console.log(querySnapshot.docs);
+  } else {
+    setupPosts([])
+  }
   loginCheck(user);
-  // if (user) {
-  //   loginCheck(user);
-  // } else {
-
-  // }
 });
